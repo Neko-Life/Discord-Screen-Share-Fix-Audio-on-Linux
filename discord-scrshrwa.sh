da974eedcc3b2333e2e47d4de17246cb3819479c
beyond9thousand <77363063+beyond9thousand@users.noreply.github.com>
2022-06-06 09:34:24 +0530

    Update and rename discord-scrshrwa.sh to pipewire.conf

--- discord-scrshrwa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pactl load-module module-null-sink sink_name=V1 sink_properties=device.description=mic+app
-pactl load-module module-null-sink sink_name=V2 sink_properties=device.description=app
-pactl load-module module-loopback source=@DEFAULT_SOURCE@ sink=V1
-pactl load-module module-loopback source=V2.monitor sink=V1
-pactl load-module module-loopback source=V2.monitor sink=@DEFAULT_SINK@
